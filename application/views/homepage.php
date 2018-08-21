611c26affcbf5bb1fd734a85e578c5038b7e5197
listiani13 <listiliz13@gmail.com>
2018-08-21 22:09:49 +0700

    config, homepage, about research

---
+++ application/views/homepage.php
@@ -0,0 +1,140 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');?>
+<?php echo $header; ?>
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>Computer Assisted and Systematic Methodology for Detection Of Chronic Lower Back Pain (CLBP) Using Artifical Intelligence and Computer Grapichs Technologies</h1>
+					<p> Research Team:<br>
+					Dr. Friska Natalia,  M.T. <br>
+					Hira Meidia B.Eng, Ph.D <br>
+					Nunik Afriliana S.Kom., MMSI <br>
+					Dr. Sud Sudirman B.Eng (Hons) PhD FHEA<br />
+					</p>
+				</div>
+				<video autoplay loop muted playsinline src="<?php echo base_url(); ?>assets/images/banner.mp4"></video>
+			</section>
+
+		<!-- Highlights -->
+			<section class="wrapper">
+				<div class="inner">
+					<div class="highlights">
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-search"><span class="label">Icon</span></a>
+									<h3>About Research</h3>
+								</header>
+								<p>This research is a collaborative project between Universitas Multimedia Nusantara (Indonesia) and Liverpool John Moores University (United Kingdom) in the area of medical image analysis and processing.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-line-chart"><span class="label">Icon</span></a>
+									<h3>Project Phases</h3>
+								</header>
+								<p>This section describes the research roadmap and the association with the lead researcher as the proposer in conducting this study described in the future research road map with the long-term scale. </p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-files-o"><span class="label">Icon</span></a>
+									<h3>Publications</h3>
+								</header>
+								<p>This section describes research outputs, including research reports each year, scientific publications (International Journal Scopus Indexed each year and International Conference each year), and the presence of methods and algorithms in the second year.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-database"><span class="label">Icon</span></a>
+									<h3>Data</h3>
+								</header>
+								<p>Visitors can preview and download research data.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-paper-plane-o"><span class="label">Icon</span></a>
+									<h3>Results</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-angle-double-right"><span class="label">Icon</span></a>
+									<h3>Future Research</h3>
+								</header>
+								<p>This section explains future research and this project's phase 2.</p>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- CTA -->
+			<section id="cta" class="wrapper">
+				<div class="inner">
+					<div style="margin-bottom: 3rem">
+						<img src="<?php echo base_url(); ?>assets/images/logo/umn.png" width="62.5rem" height="120rem" style="margin-right: 10rem"></img>
+						<img src="<?php echo base_url(); ?>assets/images/logo/kemenristekdikti.png" width="111rem" height="120rem" style=""></img>
+					</div>
+					<p style="font-size: 2rem">The study is conducted by grant from Indonesian Ministry of Research, Technology and Higher Education 034/KM/PNT/2018.</p>
+				</div>
+			</section>
+
+		<!-- Testimonials -->
+			<section class="wrapper">
+				<div class="inner">
+					<header class="special">
+						<h2>Research Team</h2>
+					</header>
+					<div class="testimonials">
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Friska Natalia received her Ph.D. in Industrial Engineering from Kyungsung University, Busan, Republic of Korea. She was Post-Doctoral felow with Pusan National University, Busan, Republic of Korea.
+									</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url(); ?>assets/images/pic01.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>Jane Doe</strong> <span>CEO - ABC Inc.</span></p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url(); ?>assets/images/pic03.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>John Doe</strong> <span>CEO - ABC Inc.</span></p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url(); ?>assets/images/pic02.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>Janet Smith</strong> <span>CEO - ABC Inc.</span></p>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
+<?php echo $footer; ?>
