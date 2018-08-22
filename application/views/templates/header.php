6053c4b74cc2487e27d41c710ceb5043b272f87c
listiani13 <listiliz13@gmail.com>
2018-08-22 15:40:23 +0700

    Edit menu link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,13 +20,13 @@
 			<header id="header">
 				<a class="logo" href="index.html">CLBP</a>
 				<nav>
-					<a href="#menu">Home</a>
-					<a href="#menu">About Research</a>
-					<a href="#menu">Project Phases</a>
-					<a href="#menu">Publications</a>
-					<a href="#menu">Data</a>
-					<a href="#menu">Results</a>
-					<a href="#menu">Future Research</a>
+					<a href="<?php echo base_url(); ?>homepage">Home</a>
+					<a href="<?php echo base_url(); ?>aboutResearch">About Research</a>
+					<a href="<?php echo base_url(); ?>projectPhases">Project Phases</a>
+					<a href="<?php echo base_url(); ?>publications">Publications</a>
+					<a href="<?php echo base_url(); ?>data">Data</a>
+					<a href="<?php echo base_url(); ?>results">Results</a>
+					<a href="<?php echo base_url(); ?>futureResearch">Future Research</a>
 				</nav>
 			</header>
 
